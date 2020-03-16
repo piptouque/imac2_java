a08c6a97fee0d69a469ccbc9c2543e2ea166e4e5
piptouque <piptouque>
2020-03-16 14:47:33 +0100

    Revert "Uploading and update"
    Oops, I did it again.
    I played with checkout.
    I got lost in the git.

--- fr/umlv/td1/AfficherArguments.java
+++
@@ -1,7 +0,0 @@
-package fr.umlv.td1;
-
-public class AfficherArguments {
-	public static void main(String args[]) {
-		for(String arg: args)  System.out.println(arg);
-	}
-}
--- fr/umlv/td1/HelloGroland.java
+++
@@ -1,7 +0,0 @@
-package fr.umlv.td1;
-
-public class HelloGroland {
-	public static void main(String args[]) {
-		System.out.println("Hello Groland");
-	}
-}
